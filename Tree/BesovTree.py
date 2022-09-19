a927970ba6de58de638aa5407630fd638c711692
kanarian <arian.joyandeh@hotmail.com>
2022-09-19 15:01:50 +0200

    Fix Besov Tree and tests

--- Tree/BesovTree.py
+++ Tree/BesovTree.py
@@ -1,88 +1,102 @@
 from dataclasses import dataclass
 from queue import Queue
 import math
-
 @dataclass
 class BesovTree:
     """Random Tree"""
     wavelet_coefficients: dict
     beta: float
 
+
     def getParentIndex(self, j, k):
         assert j > 0, f"Node on level {j} has no parent"
         return j - 1, math.floor(k / 2)
 
-    def getLeftIndex(self, j, k, max_depth):
-        assert j + 1 <= max_depth, "Node on max_depth has no left child"
+    def getLeftIndex(self, j, k):
+        assert j + 1 <= self.j_max, "Node on max_depth has no left child"
         return j + 1, 2 * k
 
-    def getRightIndex(self, j, k, max_depth):
-        assert j + 1 <= max_depth, "Node on max_depth has no right child"
+    def getRightIndex(self, j, k):
+        assert j + 1 <= self.j_max, "Node on max_depth has no right child"
         return j + 1, 2 * k + 1
 
-    def mForSubtree(self, j, k, max_depth):
+    def mForSubtree(self, j, k):
         sum = 0
         thisQueue = Queue()
         thisQueue.put((j, k))
         while not thisQueue.empty():
             currJ, currK = thisQueue.get()
-            sum += self.wavelet_coefficients[currJ, currK]
-            if currJ + 1 <= max_depth:
-                thisQueue.put(self.getLeftIndex(currJ, currK, max_depth))
-                thisQueue.put(self.getRightIndex(currJ, currK, max_depth))
+            sum += abs(self.wavelet_coefficients[currJ, currK])**2
+            if currJ + 1 <= self.j_max:
+                thisQueue.put(self.getLeftIndex(currJ, currK))
+                thisQueue.put(self.getRightIndex(currJ, currK))
         return sum
 
-    def __post_init__(self):
-        j_max = int(math.log2(len(self.wavelet_coefficients)))
+    def initializeLeafs(self):
+        for k in range(0, 2 ** self.j_max):
+            self.F[(self.j_max, k)] = self.calcF(self.j_max, k)
 
-        t = {}
-        F = {}
-        for k in range(0, 2 ** j_max):
-            F[(j_max, k)] = .25 * abs(self.wavelet_coefficients[j_max, k]) ** 2
+    def calcF(self, j, k):
+        return .25 * abs(self.wavelet_coefficients[j, k]) ** 2
 
-        j = j_max - 1
-        while j >= 0:
-            s = 2 ** (j_max - j + 1) - 1
-            for k in range(0, 2 ** j):
-                F[(j, k)] = .25 * abs(self.wavelet_coefficients[j, k]) ** 2
-                j_l, k_l = self.getLeftIndex(j, k, max_depth=j_max)
-                if F[j_l, k_l] - math.log(self.beta) < .5 * abs(
-                        self.mForSubtree(j_l, k_l, max_depth=j_max)) ** 2 - s * math.log(1 - self.beta):
-                    t[j_l, k_l] = 1
-                    F[j, k] = F[j, k] + F[j_l, k_l] - math.log(self.beta)
-                else:
-                    t[j_l, k_l] = 0
-                    F[j, k] = F[j, k] + .5 * abs(self.mForSubtree(j_l, k_l, max_depth=j_max)) ** 2 - s * math.log(
-                        1 - self.beta)
+    def getNumberOfNodesInSubtree(self, j):
+        return 2 ** (self.j_max - j + 1) - 1
 
-                j_r, k_r = self.getRightIndex(j, k, max_depth=j_max)
-                if F[j_r, k_r] - math.log(self.beta) < .5 * abs(
-                        self.mForSubtree(j_r, k_r, max_depth=j_max)) ** 2 - s * math.log(
-                        1 - self.beta):
-                    t[j_r, k_r] = 1
-                    F[j, k] = F[j, k] + F[j_r, k_r] - math.log(self.beta)
-                else:
-                    t[j_r, k_r] = 0
-                    F[j, k] = F[j, k] + .5 * abs(self.mForSubtree(j_r, k_r, max_depth=j_max)) ** 2 - s * math.log(
-                        1 - self.beta)
-            j = j - 1
+    def considerSubTree(self, j, k, child_index):
+        assert child_index == 0 or child_index == 1, "child_index must be 0 or 1"
+        s = self.getNumberOfNodesInSubtree(j + 1)
+        if child_index == 0:
+            j_c, k_c = self.getLeftIndex(j, k)
+        else:
+            j_c, k_c = self.getRightIndex(j, k)
+        subTreeVal = .5 * self.mForSubtree(j_c, k_c)
+        if self.F[j_c, k_c] - math.log(self.beta) < subTreeVal - s * math.log(1 - self.beta):
+            self.t[j_c, k_c] = 1
+            self.F[j, k] = self.F[j, k] + self.F[j_c, k_c] - math.log(self.beta)
+        else:
+            self.t[j_c, k_c] = 0
+            self.F[j, k] = self.F[j, k] + subTreeVal - s * math.log(1 - self.beta)
 
+    def createConnectedTree(self):
         t_tilde = {}
+
         t_tilde[(0, 0)] = 1
-        for j in range(1, j_max + 1):
+        for j in range(1, self.j_max + 1):
             for k in range(0, 2 ** j - 1 + 1):
                 j_p, k_p = self.getParentIndex(j, k)
-                t_tilde[(j, k)] = t[j, k] * t_tilde[j_p, k_p]
+                t_tilde[(j, k)] = t_tilde[j_p, k_p] if self.t[(j, k)] == 1 else 0
+        return t_tilde
 
+    def getMinimizingCoefficients(self, t_tilde):
         g = {}
-        for j in range(0, j_max + 1):
+        for j in range(0, self.j_max + 1):
             for k in range(0, 2 ** j - 1 + 1):
                 if abs(t_tilde[j, k] - 1) < 1e-10:
-                    g[j,k] = self.wavelet_coefficients[j,k]/2
+                    # g[j,k] = self.wavelet_coefficients[j,k]*.5
                     g[j, k] = self.wavelet_coefficients[j, k]
                 else:
                     g[j, k] = 0
-        self.transformedValues = g
+        return g
 
-    def getTransformedCoefficients(self):
-        return self.transformedValues
+    def getMaxDepth(self):
+        return int(math.log2(len(self.wavelet_coefficients)))
+
+    def getMinimizingPosteriorCoefficients(self):
+        self.initializeLeafs()
+
+        j = self.j_max - 1
+        while j >= 0:
+            for k in range(0, 2 ** j):
+                self.F[(j, k)] = self.calcF(j, k)
+                self.considerSubTree(j, k, 0)
+                self.considerSubTree(j, k, 1)
+            j = j - 1
+
+        t_tilde = self.createConnectedTree()
+        g = self.getMinimizingCoefficients(t_tilde)
+        return g
+
+    def __post_init__(self):
+        self.j_max = self.getMaxDepth()
+        self.t = {}
+        self.F = {}
--- Tree/test_BesovTree.py
+++ Tree/test_BesovTree.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from BesovTree import BesovTree
+import math
 
 
 class TestBesovTree(TestCase):
@@ -28,33 +29,128 @@ class TestBesovTree(TestCase):
         wavelet_dict = {(0, 0): 1, (1, 0): 1, (1, 1): 1}
         beta = 0.5
         besov_tree = BesovTree(wavelet_dict, beta)
-        assert(besov_tree.getLeftIndex(0, 0, 1) == (1, 0))
+        assert(besov_tree.getLeftIndex(0, 0) == (1, 0))
 
     def test_get_left_index_throws(self):
         wavelet_dict = {(0, 0): 1, (1, 0): 1, (1, 1): 1}
         beta = 0.5
         besov_tree = BesovTree(wavelet_dict, beta)
         with self.assertRaises(AssertionError):
-            besov_tree.getLeftIndex(1, 1, 1)
+            besov_tree.getLeftIndex(1, 1)
 
     def test_get_right_index(self):
         wavelet_dict = {(0, 0): 1, (1, 0): 1, (1, 1): 1}
         beta = 0.5
         besov_tree = BesovTree(wavelet_dict, beta)
-        assert(besov_tree.getRightIndex(0, 0, 1) == (1, 1))
+        assert(besov_tree.getRightIndex(0, 0) == (1, 1))
 
     def test_get_right_index_throws(self):
         wavelet_dict = {(0, 0): 1, (1, 0): 1, (1, 1): 1}
         beta = 0.5
         besov_tree = BesovTree(wavelet_dict, beta)
         with self.assertRaises(AssertionError):
-            besov_tree.getRightIndex(1, 1, 1)
+            besov_tree.getRightIndex(1, 1)
 
     def test_m_for_subtree(self):
+        wavelet_dict = {(0, 0): 1.2, (1, 0): 0.3, (1, 1): -0.4}
+        beta = 0.5
+        besov_tree = BesovTree(wavelet_dict, beta)
+        self.assertAlmostEqual(besov_tree.mForSubtree(0, 0), (1.2**2+0.3**2+0.4**2) ,delta=0.0001)
+
+    def test_get_max_depth(self):
         wavelet_dict = {(0, 0): 1, (1, 0): 1, (1, 1): 1}
         beta = 0.5
         besov_tree = BesovTree(wavelet_dict, beta)
-        assert(besov_tree.mForSubtree(0, 0,1) == 3)
+        assert(besov_tree.getMaxDepth() == 1)
 
-    def test_get_transformed_coefficients(self):
-        self.fail()
+    def test_calcF(self):
+        wavelet_dict = {(0, 0): 1, (1, 0): 1, (1, 1): 1}
+        beta = 0.5
+        besov_tree = BesovTree(wavelet_dict, beta)
+        self.assertAlmostEqual(besov_tree.calcF(0, 0),0.25,delta=0.0001)
+
+    def test_initializeLeafsCorrectLength(self):
+        wavelet_dict = {(0, 0): 1, (1, 0): 1, (1, 1): 1}
+        beta = 0.5
+        besov_tree = BesovTree(wavelet_dict, beta)
+        besov_tree.initializeLeafs()
+        assert(len(besov_tree.F) == 2)
+
+    def test_initializeLeafsCorrectValues(self):
+        wavelet_dict = {(0, 0): 1, (1, 0): 1, (1, 1): 1}
+        beta = 0.5
+        besov_tree = BesovTree(wavelet_dict, beta)
+        besov_tree.initializeLeafs()
+        assert(besov_tree.F[(1, 0)] == .25)
+        assert(besov_tree.F[(1, 1)] == .25)
+
+    def test_considerLeftSubtree(self):
+        wavelet_dict = {(0, 0): 1.2, (1, 0): 0.3, (1, 1): 0.4}
+        beta = 0.2
+        besov_tree = BesovTree(wavelet_dict, beta)
+        # initializing leafs:
+        besov_tree.initializeLeafs()
+        besov_tree.F[(0, 0)] = besov_tree.calcF(0, 0)
+        besov_tree.considerSubTree(0, 0, 0)
+        self.assertAlmostEqual(besov_tree.F[(0, 0)], 0.25*1.2**2 + 0.5*0.3**2 - math.log(1-beta), delta=0.0001)
+        # left has not been chosen so the left child should have t[(1,0)] = 0
+        self.assertEqual(besov_tree.t[(1, 0)], 0)
+
+    def test_considerRightSubtree(self):
+        wavelet_dict = {(0, 0): 1.2, (1, 0): 0.3, (1, 1): 0.4}
+        beta = 0.2
+        besov_tree = BesovTree(wavelet_dict, beta)
+        # initializing leafs:
+        besov_tree.initializeLeafs()
+        besov_tree.F[(0, 0)] = besov_tree.calcF(0, 0)
+        besov_tree.considerSubTree(0, 0, 1)
+        self.assertAlmostEqual(besov_tree.F[(0, 0)], 0.25*1.2**2 + 0.5*0.4**2 - math.log(1-beta), delta=0.0001)
+        # left has not been chosen so the left child should have t[(1,0)] = 0
+        self.assertEqual(besov_tree.t[(1, 1)], 0)
+
+    def test_considerBothSubtrees(self):
+        wavelet_dict = {(0, 0): 1.2, (1, 0): 0.3, (1, 1): 0.4}
+        beta = 0.2
+        besov_tree = BesovTree(wavelet_dict, beta)
+        # initializing leafs:
+        besov_tree.initializeLeafs()
+        besov_tree.F[(0, 0)] = besov_tree.calcF(0, 0)
+        besov_tree.considerSubTree(0, 0, 0)
+        besov_tree.considerSubTree(0, 0, 1)
+        self.assertAlmostEqual(besov_tree.F[(0, 0)], 0.25*1.2**2 + 0.5*0.4**2 - math.log(1-beta) + 0.5*0.3**2 - math.log(1-beta), delta=0.0001)
+
+        #none of the leafs were chosen
+        self.assertEqual(besov_tree.t[(1, 1)], 0)
+        self.assertEqual(besov_tree.t[(1, 0)], 0)
+
+    def test_considerSubtreeLeftAdd(self):
+        wavelet_dict = {(0, 0): 1.2, (1, 0): 0.3, (1, 1): 0.4}
+        beta = 0.9
+        besov_tree = BesovTree(wavelet_dict, beta)
+        besov_tree.initializeLeafs()
+        besov_tree.F[(0, 0)] = besov_tree.calcF(0, 0)
+        besov_tree.considerSubTree(0, 0, 0)
+        self.assertAlmostEqual(besov_tree.F[(0, 0)], 0.25*1.2**2 + 0.25*0.3**2 - math.log(beta), delta=0.0001)
+        self.assertEqual(besov_tree.t[(1, 0)], 1)
+
+    def test_considerSubtreeRightAdd(self):
+        wavelet_dict = {(0, 0): 1.2, (1, 0): 0.3, (1, 1): 0.4}
+        beta = 0.9
+        besov_tree = BesovTree(wavelet_dict, beta)
+        besov_tree.initializeLeafs()
+        besov_tree.F[(0, 0)] = besov_tree.calcF(0, 0)
+        besov_tree.considerSubTree(0, 0, 1)
+        self.assertAlmostEqual(besov_tree.F[(0, 0)], 0.25*1.2**2 + 0.25*0.4**2 - math.log(beta), delta=0.0001)
+        self.assertEqual(besov_tree.t[(1, 1)], 1)
+
+    def test_considerSubtreeBothAdd(self):
+        wavelet_dict = {(0, 0): 1.2, (1, 0): 0.3, (1, 1): 0.4}
+        beta = 0.9
+        besov_tree = BesovTree(wavelet_dict, beta)
+        besov_tree.initializeLeafs()
+        besov_tree.F[(0, 0)] = besov_tree.calcF(0, 0)
+        besov_tree.considerSubTree(0, 0, 0)
+        besov_tree.considerSubTree(0, 0, 1)
+        self.assertAlmostEqual(besov_tree.F[(0, 0)], 0.25*1.2**2 + 0.25*0.4**2 - math.log(beta) + + 0.25*0.3**2 - math.log(beta), delta=0.0001)
+        self.assertEqual(besov_tree.t[(1, 1)], 1)
+        self.assertEqual(besov_tree.t[(1, 0)], 1)
